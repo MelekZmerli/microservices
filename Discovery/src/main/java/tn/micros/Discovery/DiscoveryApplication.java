d4926f217170e645af3a8408b949a0b922a9cc9d
Malek Zmerli <zmerlimelek@gmail.com>
2022-04-12 23:15:41 +0100

    created a remote configuration server

--- Discovery/src/main/java/tn/micros/Discovery/DiscoveryApplication.java
+++ Discovery/src/main/java/tn/micros/Discovery/DiscoveryApplication.java
@@ -2,11 +2,15 @@ package tn.micros.Discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RefreshScope
 @EnableEurekaServer
+@RestController
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
---
+++ Figura/src/test/java/tn/micros/Figura/FiguraApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.micros.Figura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
