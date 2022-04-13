081745c7e2576f096f5767c15e251e083001ab17
Malek Zmerli <zmerlimelek@gmail.com>
2022-04-13 18:05:54 +0100

    created feign client -not finished-

---
+++ Sham/src/test/java/tn/micros/Sham/ShamApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.micros.Sham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
